62aeb7cc71f13f23d64f0762b1ece2cbc5a04016
fedezam <samprasfed@gmail.com>
2025-03-08 09:57:12 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,23 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAgIPuOIfvYp191JZI9cKLRkKXfGwdaCxM",
   authDomain: "trueque-28b33.firebaseapp.com",
   projectId: "trueque-28b33",
-  storageBucket: "trueque-28b33.firebasestorage.app",
+  storageBucket: "trueque-28b33.appspot.com",
   messagingSenderId: "6430433157",
   appId: "1:6430433157:web:1e6cf47ee1ed80b127eeec",
   measurementId: "G-JMDRX032BS"
 };
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
 
-// Exportar la instancia de autenticación y Firestore
-export const auth = firebase.auth();
-export const db = firebase.firestore();
+auth.useDeviceLanguage(); // 📌 Asegura idioma correcto en Firebase UI
+
+export { app, auth, db, googleProvider };
